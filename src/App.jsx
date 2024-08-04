cc605f189dc28ae2d907d327fa724dfbde475aa8
Fardeen <fardeenclan@gmail.com>
2024-08-04 23:03:40 +0530

    added more UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
-
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   );
 };
